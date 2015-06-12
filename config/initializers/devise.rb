d4f64c1ea0396b9ccefb94ea7552ba40fe68efd6
nagane <naganeys@gmail.com>
2015-06-13 02:17:31 +0900

    execute rails g model SocialProfile user:references provider uid name nickname email url image_url description other:text credentials:text raw_info:text

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,10 +1,9 @@
 Devise.setup do |config|
-  OAUTH_CONFIG = YAML.load_file("#{Rails.root}/config/omniauth.yml")[Rails.env].symbolize_keys!
+  #OAUTH_CONFIG = YAML.load_file("#{Rails.root}/config/omniauth.yml")[Rails.env].symbolize_keys!
 
   # https://github.com/intridea/omniauth-github
   # http://developer.github.com/v3/oauth/
   # http://developer.github.com/v3/oauth/#scopes
-  config.omniauth :github, OAUTH_CONFIG[:github]['key'], OAUTH_CONFIG[:github]['secret'], scope: 'user,public_repo'
-
+  #config.omniauth :github, OAUTH_CONFIG[:github]['key'], OAUTH_CONFIG[:github]['secret'], scope: 'user,public_repo'
 
 end
